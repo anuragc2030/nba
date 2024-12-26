770b6fe67b1f962a888298686f4ab424878a5c69
anuragc2030 <anuragc2030@gmail.com>
2024-12-25 23:19:03 -0800

    fixed a bug

--- multiplicative_weight_update.py
+++ multiplicative_weight_update.py
@@ -6,9 +6,9 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn import datasets, linear_model
-#import tensorflow as tf
-#from tf.keras.models import Sequential
-#from tf.keras.layers import Dense
+import tensorflow as tf
+from tf.keras.models import Sequential
+from tf.keras.layers import Dense
 
 
 file_path = r'C:\Users\anura\Downloads\2023-2024 NBA Player Stats - Playoffs.csv'
@@ -25,61 +25,59 @@ iterations = 10
 
 linear = linear_model.LinearRegression()
 
-#model_nn = Sequential([
-#    Dense(128, activation='relu', input_shape=(X_train.shape[1],)),
-#    Dense(64, activation='relu'),
-#    Dense(32, activation='relu'),
-#    Dense(1)
-#])
-#model_nn.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
-#model_nn.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2, verbose=0)
-
-
+model_nn = Sequential([
+    Dense(128, activation='relu', input_shape=(X_train.shape[1],)),
+    Dense(64, activation='relu'),
+    Dense(32, activation='relu'),
+    Dense(1)
+])
+model_nn.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+model_nn.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2, verbose=0)
 
 model_rf = RandomForestRegressor(n_estimators=100, random_state=42)
-#model_rf.fit(X_train, y_train)
+model_rf.fit(X_train, y_train)
 
 model_dt = DecisionTreeRegressor(random_state=42)
-#model_dt.fit(X_train, y_train)
+model_dt.fit(X_train, y_train)
 linear.fit(X_train, y_train)
 pred_linear = linear.predict(X_test)
 loss_linear = mean_squared_error(y_test, pred_linear)
 
-    # Train Model 2 
-   # pred_nn = model_nn.predict(X_test).flatten()
-   # loss_nn = mean_absolute_error(y_test, pred_nn)
+# Train Model 2 
+pred_nn = model_nn.predict(X_test).flatten()
+loss_nn = mean_absolute_error(y_test, pred_nn)
 
-    # Train Model 3 
+# Train Model 3 
 model_rf.fit(X_train, y_train)
 pred_rf = model_rf.predict(X_test)
 loss_rf = mean_squared_error(y_test, pred_rf)
+
  # Train Model 4 
 model_dt.fit(X_train, y_train)
 pred_dt = model_dt.predict(X_test)
 loss_dt = mean_squared_error(y_test, pred_dt)
 
-    # Compute the absolute errors for each prediction
+# Compute the absolute errors for each prediction
 errors_linear = np.abs(y_test - pred_linear)
-   # errors_nn = np.abs(y_test - pred_nn)
+errors_nn = np.abs(y_test - pred_nn)
 errors_rf = np.abs(y_test - pred_rf)
 errors_dt = np.abs(y_test - pred_dt)
 
-    # Update weights
+# Update weights
 weights[0] *= np.prod(1 / (2 ** errors_linear))
-  # weights[1] *= np.prod(1 / (2 ** errors_nn))
+weights[1] *= np.prod(1 / (2 ** errors_nn))
 weights[2] *= np.prod(1 / (2 ** errors_rf))
 weights[3] *= np.prod(1 / (2 ** errors_dt))
 
 weights /= weights.sum()
 
-for i in range(iterations):
-    final_predictions = (weights[0] * pred_linear + 
-                        # weights[1] * pred_nn + 
-                         weights[2] * pred_rf + 
-                         weights[3] * pred_dt)/3
+final_predictions = (weights[0] * pred_linear + 
+                     weights[1] * pred_nn + 
+                     weights[2] * pred_rf + 
+                     weights[3] * pred_dt)/3
 
-    # Evaluate the model
-    final_mae = mean_squared_error(y_test, final_predictions)
-    print(f'Iteration {i+1}, Final Test MSE: {final_mae}')
+# Evaluate the model
+final_mae = mean_squared_error(y_test, final_predictions)
+print(f'Iteration {i+1}, Final Test MSE: {final_mae}')
 
 print(f'Final Weights: {weights}')
